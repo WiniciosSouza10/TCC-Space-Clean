d570e701fb7d1bd2ba6e9d1d6bf315f44c155518
Winicios souza <125080576+WiniciosSouza@users.noreply.github.com>
2023-03-10 16:59:28 -0300

    Adiciona Prefabs sem textura

---
+++ TCC-Space-Clean-Game/Assets/Scripts/MovimentoNave.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoNave : MonoBehaviour
+{
+
+    private Rigidbody2D rbPlayer;
+
+    private float movimentoPlayerX;
+    public float movimentoPlayerY;
+
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimentoPlayerX = Input.GetAxis("Horizontal");
+        movimentoPlayerY = Input.GetAxis("Horizontal");
+
+        transform.Translate(movimentoPlayerX, movimentoPlayerY, movimentoPlayerY * speed * Time.deltaTime);
+
+    }
+}
